7a76f0026d8b4317cb0fb4810e4c7cba3d50837f
Shobhit Yadav <shobhit999000@gmail.com>
2020-12-28 01:53:52 +0530

    http requests configured

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,22 +6,43 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-
+    state ={
+        posts: [],
+        selectedPostId: null
+    }
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-            console.log(response);
+            const posts=response.data.slice(0,4);
+            const updatedPosts=posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Max'
+                }
+            })
+            this.setState({posts: updatedPosts});    
+        // console.log(response);
         });
     }
+
+    postSelectedHandler =(id) => {
+        this.setState({selectedPostId: id});
+    }
     render () {
+        const posts=this.state.posts.map(post => {
+            return <Post 
+            title={post.title} 
+            key={post.id} 
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+            />;
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
