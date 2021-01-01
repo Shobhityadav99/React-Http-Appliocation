2d2fe4a3877f74b27a32c61600b7756f476ea8e2
Shobhit Yadav <shobhit999000@gmail.com>
2021-01-01 21:02:32 +0530

    New page for full post

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,6 +2,7 @@ import React , { Component } from "react";
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
+import { Link } from 'react-router-dom';
 class Posts extends Component {
     state ={
         posts: [],
@@ -25,12 +26,15 @@ class Posts extends Component {
     }
     render() {
         let posts=this.state.posts.map(post => {
-            return <Post 
-            title={post.title} 
-            key={post.id} 
+            return (
+            <Link to={'/'+post.id} 
+            key={post.id} >
+            <Post 
+            title={post.title}
             author={post.author}
             clicked={() => this.postSelectedHandler(post.id)}
-            />;
+            />
+            </Link>);
         })
         return (
             <section className="Posts">
