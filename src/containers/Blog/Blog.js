ad2468369bfabebba643db230a73889b898dc714
Shobhit Yadav <shobhit999000@gmail.com>
2021-01-01 20:12:42 +0530

    NavLink added

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route , Link } from 'react-router-dom';
+import { Route , NavLink } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from '../Blog/NewPost/NewPost';
@@ -14,12 +14,12 @@ class Blog extends Component {
                         <ul>
                             {/* <li><a href="/">Home</a></li>
                             <li><a href="/new-post">New Post</a></li> */}
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink to="/" exact activeClassName="my-active">Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: "/new-post",
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
