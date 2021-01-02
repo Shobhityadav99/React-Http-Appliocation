a4007d8a16ea12ad5808f24d6da4381e56e584fb
Shobhit Yadav <shobhit999000@gmail.com>
2021-01-02 18:35:57 +0530

    Router added

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route , NavLink, Switch } from 'react-router-dom';
+import { Route , NavLink, Switch , Redirect } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from '../Blog/NewPost/NewPost';
@@ -29,6 +29,7 @@ class Blog extends Component {
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" exact component={NewPost} />
                 <Route path="/:id" exact component={FullPost} />
+                <Redirect from="/" to="/posts"/>
                 </Switch>
             </div>
         );
