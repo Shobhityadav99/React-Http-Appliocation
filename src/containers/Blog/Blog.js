e376d6b2e8f39a973592ae9b307cdb72213f56ef
Shobhit Yadav <shobhit999000@gmail.com>
2021-01-03 18:34:00 +0530

    Highlighted active page and redirect added

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route , NavLink, Switch , Redirect } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from '../Blog/NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render () {
@@ -13,23 +13,29 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            {/* <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li> */}
-                            <li><NavLink to="/" exact activeClassName="my-active"
-                            activeStyle={{color : '#fa923f',textDecoration: 'underline'}}>Home</NavLink></li>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
                             <li><NavLink to={{
-                                pathname: "/new-post",
+                                pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
                             }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" exact component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
-                <Redirect from="/" to="/posts"/>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="/posts" />
+                    {/* <Route path="/" component={Posts} /> */}
                 </Switch>
             </div>
         );
